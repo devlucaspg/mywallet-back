88333b4de98cabcd9fa4e7a11274ef5610588746
devlucaspg <110506094+devlucaspg@users.noreply.github.com>
2022-11-21 17:24:45 -0400

    final

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(authRoutes);
 app.use(transactionRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running in port: ${process.env.PORT}`);
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log(`Server running in port: ${port}`);
 });
